01f41bc3652d445905a870a245f22e69ebaffa0d
Bohdan Kulchytskyy <bohdan.kul@gmail.com>
2015-06-29 14:26:35 -0700

    Spin class

---
+++ spin.cpp
@@ -0,0 +1,84 @@
+#include "spin.h"
+
+
+/*****************************************************************************
+ * Initializes spins in a random state without underlying geometry
+ *****************************************************************************/
+Spins::Spins(int _Nspins, long _seed)
+/*
+    _Nspins - sets the size of spins vectors
+    _seed   - initializes the random number generator necessary 
+              to generate a random distribution of spins
+*/
+{
+    RandomBase  rand(_seed);     // random numbers generator
+   
+    // Initialize spins randomly with values 1 and -1
+    Nspins = _Nspins;
+    spins.resize(Nspins,0);
+    for (auto spin=spins.begin(); spin!=spins.end(); spin++) {                          
+        *spin = pow(-1,rand.uRandInt()%2);                         
+    }
+
+    cout << "---Spins object initialization---" << endl;
+    cout << "   # spins: " << Nspins << endl << "   ";
+    for (auto spin=spins.begin(); spin!=spins.end(); spin++) {                          
+        cout << *spin << " "; 
+    }
+    cout << endl << endl;
+
+}
+
+
+/*****************************************************************************
+ * Returns the spin state 
+ *****************************************************************************/
+int Spins::getSpin(int index)
+/*
+   index - index of the element to be flipped  
+*/
+{
+    return spins[index];
+}
+
+/*****************************************************************************
+ * Sets the spin state to a particular value
+ *****************************************************************************/
+void Spins::setSpin(int index, int val)
+/*
+   index - index of the element to be flipped  
+   val   - the new value of the spin state
+*/
+{
+    spins[index] = val;
+}
+
+/*****************************************************************************
+ * Flips a spin
+ *****************************************************************************/
+void Spins::flip(int index)
+/*
+   index - index of the element to be flipped  
+*/
+{
+    setSpin(index, -1*getSpin(index));
+}
+
+/*****************************************************************************
+ * Print out the spins state 
+ *****************************************************************************/
+void Spins::print()
+{
+    cout << "   Spins state: " << endl;
+    for (int i=0; i!=Nspins; i++)
+        cout << getSpin(i) << " ";
+    cout << endl;
+}
+
+//*****************************************************************************
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+//*****************************************************************************
+
+
---
+++ spin.h
@@ -0,0 +1,31 @@
+#ifndef SPIN_H
+#define SPIN_H
+
+#include "randombase.h"
+#include <iostream>
+
+using namespace std;
+/******************************************************************************
+ * Base spins class. 
+ * Defines spins vector structure and basic operations on it.
+ *****************************************************************************/
+class Spins
+{
+    public:
+        Spins(int _Nspins, long _seed); // initialize spins to a random distribution of +1/-1 
+        int  getSpin(int index);           // get the spin state
+        int  getSize() {return Nspins;};   // get the total number of spins 
+        void setSpin(int index, int val);  // set the spin state
+        void flip(int index);              // flip spin
+        void print();                      // print out the spins state
+    
+        //Spins operator=(const Spins& clone)
+        //    {Nsites = clone.Nsites; spins = clone.spins; return *this;};
+
+    protected:
+        vector<int> spins;    // current spins state
+        int Nspins;           // number of spins
+};
+
+
+#endif
