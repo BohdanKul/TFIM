cbe292dee9c80dcfdd9c4ea8a75911be9c702dca
Bohdan.Kul <bohdan.kul@gmail.com>
2016-01-18 15:35:57 -0800

    Added helper header file

---
+++ helper.h
@@ -0,0 +1,16 @@
+#ifndef HELPER_CPP
+#define HELPER_CPP
+
+#include <array>
+#include <algorithm>
+
+using namespace std;
+
+//*****************************************************************************
+int flipBit(const int BitStr, const int index);
+
+//*****************************************************************************
+array<int,8> sort_indexes(const array<float, 8> &v); 
+
+
+#endif
